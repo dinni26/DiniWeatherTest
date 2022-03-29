5d86470def1b7b91696ec79e6b6c7bff6cd3387e
dinni26 <dinniwidya99@gmail.com>
2022-03-30 00:01:39 +0700

    update

--- Startup.cs
+++ Startup.cs
@@ -40,10 +40,12 @@ namespace DiniWeatherTest
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DiniWeatherTest v1"));
             }
 
+            //enable 
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DiniWeatherTest v1"));
+        
             app.UseHttpsRedirection();
 
             app.UseRouting();
